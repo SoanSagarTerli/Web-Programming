89be8a8825e69a4586bebd890ac5ea490faebf9e
SoanSagarTerli <ts24csb1a65@student.nitw.ac.in>
2025-02-14 15:22:12 +0530

    Add files via upload

---
+++ New folder/question2 28-8-24.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{cout<<"ask for degrees in celsius:";
+int C;
+cin>>C;
+cout<<"degress in farenheit is:"<<((C*(9/5))+32)<<endl;
+return 0;
+}
---
+++ New folder/question3 28-8-24.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{cout<<"ask for the radius of circle:";
+int r;
+cout<<"the area of circle is:"<<(3.14*r*r)<<endl;
+cout<<"the perimeter of the circle is:"<<(2*3.14*r)<<endl;
+return 0;
+}
