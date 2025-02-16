9336989b6b8bb5b97d2ff7ed96e58944b79f37d2
SoanSagarTerli <ts24csb1a65@student.nitw.ac.in>
2025-02-16 21:48:21 +0530

    Delete New folder directory

--- New folder/question2 28-8-24.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{cout<<"ask for degrees in celsius:";
-int C;
-cin>>C;
-cout<<"degress in farenheit is:"<<((C*(9/5))+32)<<endl;
-return 0;
-}
--- New folder/question3 28-8-24.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{cout<<"ask for the radius of circle:";
-int r;
-cout<<"the area of circle is:"<<(3.14*r*r)<<endl;
-cout<<"the perimeter of the circle is:"<<(2*3.14*r)<<endl;
-return 0;
-}
